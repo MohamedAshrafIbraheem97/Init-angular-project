a07de4999bbf74ec1c4aa0d6dfc497ead58620a9
Mohamed Ashraf Ibraheem <mohamedashrafibraheem@gmail.com>
2022-11-12 03:25:30 +0200

    added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
 
-const routes: Routes = [{ path: 'portfolio', component: PortfolioComponent }];
+const routes: Routes = [
+  { path: '', redirectTo: 'portfolio', pathMatch: 'full' },
+  { path: 'portfolio', component: PortfolioComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
